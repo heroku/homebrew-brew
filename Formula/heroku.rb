8df3790fec281f36840cac0e6858e9539ee68ba0
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-08-30 14:43:28 -0700

    heroku 6.14.3-9284403

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,8 +1,8 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.14.3-115fb1b-darwin-x64.tar.xz"
-  version "6.14.3-115fb1b"
+  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.14.3-9284403-darwin-x64.tar.xz"
+  version "6.14.3-9284403"
   sha256 "a466e66cad83e578029ff19c5f52a561cff85c6f3af4ac31d23361f16ec173a8"
 
   def install
