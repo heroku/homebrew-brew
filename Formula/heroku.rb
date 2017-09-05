445ab643cb3d0bbb997950782758cc2557f6de09
heroku-cli <31491619+heroku-cli@users.noreply.github.com>
2017-09-05 01:55:15 +0000

    heroku 6.14.16-9ae58fc

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,9 +1,9 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.14.15-a1e54d1-darwin-x64.tar.xz"
-  version "6.14.15-a1e54d1"
-  sha256 "298bd8e159ffa3f111ab321b88c1eecfe255e52a7030af9bf0c35ccd49137938"
+  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.14.16-9ae58fc-darwin-x64.tar.xz"
+  version "6.14.16-9ae58fc"
+  sha256 "87428f5fc628a25b156a033c5c23ed81e167153c2283722d5ef9794a9b22b62c"
 
   def install
     libexec.install Dir["*"]
