96cb813317ce9c6bffa2ebc10c97f599a6b85b98
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-06 13:43:08 -0700

    downgrade node temporarily to get server to bump

--- Formula/heroku-node.rb
+++ Formula/heroku-node.rb
@@ -1,7 +1,7 @@
 class HerokuNode < Formula
   desc "node.js dependency for heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/homebrew/node-8.4.0.tar.xz"
+  url "https://cli-assets.heroku.com/homebrew/node-8.3.0.tar.xz"
   sha256 "6fbfbe01ef9a7411eab9e0e4b679da8eefb495ff7e1fbd116011ee7409046ee7"
 
   def install
