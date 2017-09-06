f4150324357102d097a39fe70185b5f6d6536e45
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-06 13:03:05 -0700

    break up node into heroku-node

---
+++ Formula/heroku-node.rb
@@ -0,0 +1,14 @@
+class HerokuNode < Formula
+  desc "node.js dependency for heroku"
+  homepage "https://cli.heroku.com"
+  url "https://cli-assets.heroku.com/homebrew/node-v8.4.0.tar.xz"
+  sha256 "f6998a498626aea7717e74f7236ed0b3c7115bf24f6d966984440d7689ef78a5"
+
+  def install
+    share.install buildpath/"node"
+  end
+
+  # def test
+  #   system bin/"node", "version"
+  # end
+end
--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,11 +1,12 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-cli/channels/stable/heroku-cli-v6.14.18-0510d2d-darwin-x64.tar.xz"
-  version "6.14.18-0510d2d"
-  sha256 "185b7e2b5dcf940c7a9d918dd4cb8d056f258f392cfd41c4f41f48db59bb9448"
+  url "https://cli-assets.heroku.com/homebrew/heroku-v6.14.18.tar.xz"
+  sha256 "7d0b57a047699f488b4cdb7cf1cc3ffa58bab8e63b0284c2390b201a87c89b41"
+  depends_on "heroku-node"
 
   def install
+    inreplace "bin/heroku", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/heroku"
   end
