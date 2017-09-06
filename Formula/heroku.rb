325d7350a6534ebdf140a8d2e9f38513aa2ecd53
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-09-06 13:07:49 -0700

    fix route

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -2,8 +2,8 @@ class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
   url "https://cli-assets.heroku.com/homebrew/heroku-v6.14.18.tar.xz"
-  sha256 "7d0b57a047699f488b4cdb7cf1cc3ffa58bab8e63b0284c2390b201a87c89b41"
-  depends_on "heroku-node"
+  sha256 "e4735c07d3ad783057322103d9f1fe36bbd3bffc664b2a8cb7065fbb7bff5cf5"
+  depends_on "heroku/brew/heroku-node"
 
   def install
     inreplace "bin/heroku", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
