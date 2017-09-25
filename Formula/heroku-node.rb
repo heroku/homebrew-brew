fa45ac07c2223d0be77b2810358454d056b8f7d7
heroku-cli <31491619+heroku-cli@users.noreply.github.com>
2017-09-25 13:25:04 +0000

    heroku-node 8.5.0

--- Formula/heroku-node.rb
+++ Formula/heroku-node.rb
@@ -1,8 +1,8 @@
 class HerokuNode < Formula
   desc "node.js dependency for heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/homebrew/node-8.4.0.tar.xz"
-  sha256 "618565f349317adaa98d6fca46f819ec1fff7d19aafedf63b0098fa0da0f2fbb"
+  url "https://cli-assets.heroku.com/homebrew/node-8.5.0.tar.xz"
+  sha256 "85a955446796d2fdfb4ae994df78f22dd0517c21a1824b78393fe378e77b79fe"
 
   def install
     share.install buildpath/"node"
