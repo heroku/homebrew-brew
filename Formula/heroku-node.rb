87d056f6d04d625aa065c378cba34570a4ad7d6e
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2017-10-04 14:44:20 -0700

    Revert "heroku-node 8.5.0"
    This reverts commit 01267cbac6ca2b061ff16c4e38d0221ce43c1d06.

--- Formula/heroku-node.rb
+++ Formula/heroku-node.rb
@@ -1,8 +1,8 @@
 class HerokuNode < Formula
   desc "node.js dependency for heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/homebrew/node-8.5.0.tar.xz"
-  sha256 "2e0ccf6e8674114e0dd7e5e80a09d895427382f3636f8dac9bbf54d582b19ff6"
+  url "https://cli-assets.heroku.com/homebrew/node-8.6.0.tar.xz"
+  sha256 "9cb09e7e204ecc49598f04c4c06d5d03934ed940c6f57ac62addabfb449d8c5f"
 
   def install
     share.install buildpath/"node"
