1eb09cabd6f5da0aaeb2e45e2c9456f15059d024
Jeff Dickey <216188+jdxcode@users.noreply.github.com>
2018-04-24 12:33:46 -0700

    fix homebrew url

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,8 +1,8 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-6.16.18.tar.xz"
-  sha256 "37c00ee4af0dd1f4631f7e9a7bd19a0c9a0b75ddab45f6b11ce77d205909612d"
+  url "https://cli-assets.heroku.com/heroku-v7.0.10/heroku-v7.0.10.tar.xz"
+  sha256 "4f7ca5443fb25c2df4f9bebbcaf3ca1b71896eb5d6e6b796df1eeb8c8b39b39e"
   depends_on "heroku/brew/heroku-node"
 
   def install
