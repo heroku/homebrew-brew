3ee090d4049570a9a3f0e801a85b1a2ef41a8e4e
RasPhilCo <capt.pn@gmail.com>
2018-07-17 15:45:05 -0700

    add homebrew completions & caveat (#6)

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -10,6 +10,19 @@ class Heroku < Formula
     inreplace "bin/heroku", "\"$DIR/node\"", "#{Formula["heroku-node"].opt_share}/node"
     libexec.install Dir["*"]
     bin.install_symlink libexec/"bin/heroku"
+
+    bash_completion.install "#{libexec}/node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/bash"
+    zsh_completion.install "#{libexec}/node_modules/@heroku-cli/plugin-autocomplete/autocomplete/brew/zsh/_heroku"
+    system bin/"heroku", "autocomplete", "--refresh-cache"
+  end
+
+  def caveats; <<~EOS
+    To use the Heroku CLI's autocomplete:
+      1) Use homebrew's shell completion - https://docs.brew.sh/Shell-Completion
+        OR
+      2) Run and follow the install steps:
+          $ heroku autocomplete
+  EOS
   end
 
   test do
