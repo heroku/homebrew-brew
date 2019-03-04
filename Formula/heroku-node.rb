3e9d20a35e0a1a73ecb8b946a6ebd08dddd38b5c
heroku-cli <31491619+heroku-cli@users.noreply.github.com>
2019-03-04 21:03:03 +0000

    heroku-node 11.10.1

--- Formula/heroku-node.rb
+++ Formula/heroku-node.rb
@@ -1,8 +1,8 @@
 class HerokuNode < Formula
   desc "node.js dependency for heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/homebrew/node-11.9.0.tar.xz"
-  sha256 "a3c27b06ef0bf0f984bf37ddb1f9f1bebffab77ea52a23934b062976895237ed"
+  url "https://cli-assets.heroku.com/homebrew/node-11.10.1.tar.xz"
+  sha256 "7ef5804562b1b28cce480ed31ed20230a262c20a19ffe611b50fb1d76dec8499"
 
   def install
     share.install buildpath/"node"
