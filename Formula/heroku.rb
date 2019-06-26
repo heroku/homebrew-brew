610465ecbe9294714e4cdc544671d923c2d16861
heroku-cli <31491619+heroku-cli@users.noreply.github.com>
2019-06-26 19:44:30 +0000

    heroku v7.26.0

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,8 +1,8 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-v7.25.0/heroku-v7.25.0.tar.xz"
-  sha256 "1bf0549060d8ca7f3d5687e55480f916febd3e85229fdf01383a25668867cb40"
+  url "https://cli-assets.heroku.com/heroku-v7.26.0/heroku-v7.26.0.tar.xz"
+  sha256 "5fff790fa8ee2d7197286d9bfde46780eba77196b8e4070f814ae7558adc2743"
   depends_on "heroku/brew/heroku-node"
 
   def install
