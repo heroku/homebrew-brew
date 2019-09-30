1608a5781eac8231de8e697084a4d4a19547383b
Jason Karns <jason.karns@gmail.com>
2019-09-30 14:46:02 -0400

    heroku-node is keg-only
    closes https://github.com/heroku/homebrew-brew/issues/13

--- Formula/heroku-node.rb
+++ Formula/heroku-node.rb
@@ -4,6 +4,8 @@ class HerokuNode < Formula
   url "https://cli-assets.heroku.com/homebrew/node-12.16.2.tar.xz"
   sha256 "3be1a9fb2715f62561912d6e824f5e8dc2d64cf7e074f6c4ac8b5c9b4a836761"
 
+  keg_only "only used by heroku CLI, which explicitly requires from Cellar"
+  
   def install
     share.install buildpath/"node"
   end
