996070f7ea8fdad0905100464e56f23d5f52970c
Justin Garrison <justinleegarrison@gmail.com>
2020-05-14 17:33:06 -0700

    Initial support for linuxbrew

--- Formula/heroku.rb
+++ Formula/heroku.rb
@@ -1,8 +1,20 @@
 class Heroku < Formula
   desc "Everything you need to get started with Heroku"
   homepage "https://cli.heroku.com"
-  url "https://cli-assets.heroku.com/heroku-v7.41.1/heroku-v7.41.1.tar.xz"
-  sha256 "2b0412e8ac2400ba587bbc38c969b3785a5e9d142dc77656b2a8f33b3398348a"
+  if OS.mac?
+    url "https://cli-assets.heroku.com/heroku-v7.41.1/heroku-v7.41.1.tar.xz"
+    sha256 "2b0412e8ac2400ba587bbc38c969b3785a5e9d142dc77656b2a8f33b3398348a"
+  elsif OS.Linux?
+    if Hardware::CPU.intel?
+      # No version for the linux gz?
+      url "https://cli-assets.heroku.com/heroku-linux-x64.tar.gz"
+      sha256 "5d1ef7e843738d597cd0360d07558832e4c8c91a2a6ffac3c671bbb4b6aab87c"
+    else # arm
+      url "https://cli-assets.heroku.com/heroku-v7.41.1/heroku-v7.41.1-linux-arm.tar.gz"
+      sha256 "af69532cb9ef95ae6447a2bc7f86149ed0057748dbca4f634ec167977b48f2d0"
+    end
+  end
+
   depends_on "heroku/brew/heroku-node"
 
   def install
